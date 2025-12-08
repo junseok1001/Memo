c8e8b53bf8026fb285d2887e38ce9d1e8bfc8fce
junseok <megaIT@megaIT>
2025-12-08 18:41:03 +0900

    [학원] - 로그인 기능, session 활용 #2

---
+++ src/main/java/com/sourjelly/memo/user/domain/User.java
@@ -0,0 +1,21 @@
+package com.sourjelly.memo.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+
+@Getter
+@Setter
+public class User {
+
+    private long id;
+    private String loginId;
+    private String password;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
